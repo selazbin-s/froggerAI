d9d405e3904771c61bb6ac83071cb4ab70781b22
selazbin-s <selazbin@csu.fullerton.edu>
2021-03-21 16:58:51 -0700

    moving

--- sketch.js
+++ sketch.js
@@ -4,6 +4,11 @@ let logs = [];
 let grid = 50;
 
 let img;
+function preload()
+{
+    img=loadImage('https://cdn.friendlystock.com/wp-content/uploads/2019/07/4-chibi-kawaii-frog-cartoon-clipart.jpg');
+}
+
 
 
 function resetGame(){
@@ -15,77 +20,77 @@ function resetGame(){
 }
 
 function setup(){
+createCanvas(700,700);
+image(img,0,0);
 
-    createCanvas(500,500);
-
-    resetGame();
-
-    img=loadImage('image/frogger.png');
+   
     
-    var button = createButton("Start");
-    button.position(500, 600);
-    button.mousePressed();
+   // resetGame();
+
+    // var button = createButton("Start");
+    // button.position(500, 600);
+    // button.mousePressed();
     
-    let index = 0;
+    // let index = 0;
 
 
 
-    // ROW 1
-    for (let i = 0; i < 2; i++) {
-        let x = i * 300;
-        cars[index] = new Car(x, height - grid * 2, grid * 2, grid, 2);
-        index ++;
-    }
+    // // ROW 1
+    // for (let i = 0; i < 2; i++) {
+    //     let x = i * 300;
+    //     cars[index] = new Car(x, height - grid * 2, grid * 2, grid, 2);
+    //     index ++;
+    // }
 
-    // ROW 2
-    for (i = 0; i < 2; i++) {
-        let x = i * 200 +150;
-        cars[index] = new Car(x, height - grid * 3, grid, grid, -1.2);
-        index ++;
-    }
+    // // ROW 2
+    // for (i = 0; i < 2; i++) {
+    //     let x = i * 200 +150;
+    //     cars[index] = new Car(x, height - grid * 3, grid, grid, -1.2);
+    //     index ++;
+    // }
 
-    // ROW 3
-    for (i = 0; i < 4; i++) {
-        let x = i * 150 + 25;
-        cars[index] = new Car(x, height - grid * 4, grid, grid, 1.2);
-        index ++;
-    }
+    // // ROW 3
+    // for (i = 0; i < 4; i++) {
+    //     let x = i * 150 + 25;
+    //     cars[index] = new Car(x, height - grid * 4, grid, grid, 1.2);
+    //     index ++;
+    // }
 
-    // ROW 4
-    index = 0;
-    for (i = 0; i < 2; i++) {
-        let x = i * 250 + 100;
-        logs[index] = new Log(x, height - grid * 5, grid * 3, grid, 2.3);
-        //cars[index] = new Car(x, height - grid * 5, grid * 3, grid, 2.3);
-        index ++;
-    }
+    // // ROW 4
+    // index = 0;
+    // for (i = 0; i < 2; i++) {
+    //     let x = i * 250 + 100;
+    //     logs[index] = new Log(x, height - grid * 5, grid * 3, grid, 2.3);
+    //     //cars[index] = new Car(x, height - grid * 5, grid * 3, grid, 2.3);
+    //     index ++;
+    // }
 
-    //ROW 5
-    //intentionally left blank, rest spot
+    // //ROW 5
+    // //intentionally left blank, rest spot
 
-    // ROW 6
-    for (i = 0; i < 3; i++) {
-        let x = i * 200 + 30;
-        logs[index] = new Log(x, height - grid * 7, grid * 2, grid, -1.3);
-        //cars[index] = new Car(x, height - grid * 7, grid * 2, grid, -1.3);
-        index ++;
-    }
+    // // ROW 6
+    // for (i = 0; i < 3; i++) {
+    //     let x = i * 200 + 30;
+    //     logs[index] = new Log(x, height - grid * 7, grid * 2, grid, -1.3);
+    //     //cars[index] = new Car(x, height - grid * 7, grid * 2, grid, -1.3);
+    //     index ++;
+    // }
 
-    // ROW 7
-    for (i = 0; i < 2; i++) {
-        let x = i * 400 + 10;
-        logs[index] = new Log(x, height - grid * 8, grid * 4, grid, 0.5);
-        //cars[index] = new Car(x, height - grid * 8, grid * 4, grid, 0.5);
-        index ++;
-    }
+    // // ROW 7
+    // for (i = 0; i < 2; i++) {
+    //     let x = i * 400 + 10;
+    //     logs[index] = new Log(x, height - grid * 8, grid * 4, grid, 0.5);
+    //     //cars[index] = new Car(x, height - grid * 8, grid * 4, grid, 0.5);
+    //     index ++;
+    // }
 
-    // ROW 8
-    for (i = 0; i < 2; i++) {
-        let x = i * 400 + 10;
-        logs[index] = new Log(x, height - grid * 9, grid * 2, grid, -2.5);
-        //cars[index] = new Car(x, height - grid * 9, grid * 2, grid, -2.5);
-        index ++;
-    }
+    // // ROW 8
+    // for (i = 0; i < 2; i++) {
+    //     let x = i * 400 + 10;
+    //     logs[index] = new Log(x, height - grid * 9, grid * 2, grid, -2.5);
+    //     //cars[index] = new Car(x, height - grid * 9, grid * 2, grid, -2.5);
+    //     index ++;
+    // }
 }
 
 function drawBackground(){
@@ -140,12 +145,13 @@ function drawAnimation(){
 }
 
 function draw(){
-    background(0);
-    drawBackground();
+  
+   // background(0);
+  //  drawBackground();
 
-    image(img,0,0);
+ 
 
-    drawAnimation();
+   // drawAnimation();
 
     frog.update();
 
@@ -171,7 +177,3 @@ function keyPressed() {
 
 const imgs = []
 
-// function preload()
-// {
-//     imgs.push(loadImage('image/frogger.png'));
-// }
